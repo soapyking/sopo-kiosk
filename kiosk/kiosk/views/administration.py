31cb1d131a233aef6c76552bbaca968e2d14d12f
Chris J <soapyking@gmail.com>
2014-05-04 21:35:54 -0400

    Betterific admin login

--- kiosk/kiosk/views/administration.py
+++ kiosk/kiosk/views/administration.py
@@ -2,6 +2,6 @@ from flask import render_template
 
 from kiosk import app
 
-@app.route('/login')
+@app.route('/administration/login')
 def admin_login():
 	return render_template("admin_login.html")
