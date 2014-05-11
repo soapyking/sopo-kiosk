ea944cf100cd54472ff38cd63bdb1e124de28727
Chris J <soapyking@gmail.com>
2014-05-11 15:03:17 -0400

    Breaking out jinja templates for inheritance

--- kiosk/kiosk/views/signin.py
+++ kiosk/kiosk/views/signin.py
@@ -1,6 +1,15 @@
 from flask import render_template
+from flask import url_for
 from kiosk import app
 
-@app.route('/signin')
+@app.route('/signin', methods=["GET"])
 def emit_signin():
 	return render_template("signin.html")
+
+@app.route('/signin', methods=["POST"])
+def submit_signin():
+	return render_template(url_for("emit_user_info"))
+
+@app.route('/user_info', methods=["GET"])
+def emit_user_info():
+	pass
