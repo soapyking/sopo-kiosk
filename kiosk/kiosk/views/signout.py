1475f54408c9de62b773831ab36466af6c1c8396
Chris J <soapyking@gmail.com>
2015-10-16 20:47:49 -0400

    Updating with much feedback gained from shop

--- kiosk/kiosk/views/signout.py
+++ kiosk/kiosk/views/signout.py
@@ -7,25 +7,27 @@ from models.event import Event
 
 @app.route('/signout', methods=["GET"])
 def emit_signout():
-	''' Let user leave feedback	'''
-	return render_template("signout.html")
+    ''' Let user leave feedback    '''
+    return render_template("signout.html")
 
 @app.route('/signout', methods=["POST"])
 def submit_signout():
-	uname = request.form.get('uname')
-	feedback = request.form.get('feedback')
-	try:
-		user = User.query.filter_by(uname=uname).first()
-		if not user:
-			app.logger.warn("User by name {0} not found".format(uname))
-			raise Exception("User by name {0} not found".format(uname)) # must improve this
-		latest_signin = Signin.query.filter_by(user_id = user.id).order_by(Signin.time_in.desc()).first()
-		latest_signin.feedback = feedback
-		latest_signin.time_out = datetime.datetime.now()
-		db.session.add(latest_signin)
-		db.session.commit()
-	except:
-		db.session.rollback()
-		raise
+    uname = request.form.get('uname')
+    feedback = request.form.get('feedback')
+    try:
+        user = User.query.filter_by(uname=uname).first()
+        if not user:
+            app.logger.warn("User by name {0} not found".format(uname))
+            error_message = "User by name {0} not found, you should use the username you used when you signed in".format(uname)
+            flash(error_message)
+            raise Exception("User by name {0} not found".format(uname)) # must improve this
+        latest_signin = Signin.query.filter_by(user_id = user.id).order_by(Signin.time_in.desc()).first()
+        latest_signin.feedback = feedback
+        latest_signin.time_out = datetime.datetime.now()
+        db.session.add(latest_signin)
+        db.session.commit()
+    except:
+        db.session.rollback()
+        raise
 
-	return redirect(url_for('all_good'))
+    return redirect(url_for('all_good'))
